63fa444eb95b1eb860117c02cc208e5350d75b25
Mikawa-Sama <stevenfardoux62670@gmail.Com>
2022-10-15 00:37:47 +0200

    canvas + enregistrement dernier tirage

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const request = require('request');
 const htmlparser = require('node-html-parser')
 const data = require('./data.json')
-const canvas = require('canvas')
-const { Client, Collection, Intents, BaseGuildVoiceChannel } = require('discord.js');
+const draw = require('./draw.json')
+const Canvas = require('canvas')
+const Discord = require('discord.js')
+const { Client, Intents } = require('discord.js');
 const fs = require('fs');
+
 require('dotenv').config()
 
 
-const client = new Client({ intents: [[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES]] });
+const client = new Client({ intents: [[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]] });
 
 var aRequest = require('util').promisify(request);
 
@@ -16,19 +19,18 @@ client.once('ready', () => {
     console.log('Ready');
 
     test();
-    // setInterval(test, 8.64e+7)
+    setInterval(test, 8.64e+7)
 });
 
-client.once('messageCreate', message => {
+client.on('messageCreate', message => {
     if (message.author.bot) return;
-    if (!message.member.permissions.has('Administrator')) return;
 
-    if (message.content === "setChan") {
+    if (message.content === 'setChan') {
         data.channelEuro[message.guild.id] = message.channel.id;
-        fs.writeFile('data.json', JSON.stringify(data));
-
+        fs.writeFileSync('data.json', JSON.stringify(data));
     }
-})  
+})
+
 
 var test = async () => {
     var html = await aRequest('https://www.fdj.fr/jeux-de-tirage/euromillions-my-million/resultats');
@@ -36,19 +38,69 @@ var test = async () => {
 
     var ball = root.querySelectorAll('.game-ball');
 
+    let textBall = "";
+    let textStar1 = "";
+    let textStar2 = "";
     ball.forEach(b => {
         console.log(b.text);
-        console.log(b.classList.contains('is-special'));
+        if (b.classList.contains('is-special') && textStar1 == "") {
+            textStar1 += b.text;
+        }
+        else if (b.classList.contains('is-special') && textStar1 !== "") {
+            textStar2 += b.text;
+        }
+        else {
+            textBall += `${b.text}    `;
+        }
     });
 
-    for (const guildID in data.channelEuro) {
-        if (Object.hasOwnProperty.call(data.channelEuro, guildID)) {
-            const channelID = data.channelEuro[guildID];
-            client.channels.cache.get(channelID).send("ok");
-            
+    fs.readFile('draw.json', async (err, d) => {
+        if (err) throw err;
+        let readData = JSON.parse(d);
+
+
+        if (ball[0].text != readData.lastDraw.ball1 || ball[1].text != readData.lastDraw.ball2 || ball[2].text != readData.lastDraw.ball3 || ball[3].text != readData.lastDraw.ball4 || ball[4].text != readData.lastDraw.ball5 || ball[5].text != readData.lastDraw.star1 || ball[6].text != readData.lastDraw.star2) {
+            draw.lastDraw['ball1'] = ball[0].text;
+            draw.lastDraw['ball2'] = ball[1].text;
+            draw.lastDraw['ball3'] = ball[2].text;
+            draw.lastDraw['ball4'] = ball[3].text;
+            draw.lastDraw['ball5'] = ball[4].text;
+            draw.lastDraw['star1'] = ball[5].text;
+            draw.lastDraw['star2'] = ball[6].text;
+            console.log('ok')
+
+            fs.writeFileSync('draw.json', JSON.stringify(draw));
+
+            for (const guildID in data.channelEuro) {
+                const channelID = data.channelEuro[guildID];
+
+                if (Object.hasOwnProperty.call(data.channelEuro, guildID)) {
+                    const canvas = Canvas.createCanvas(1000, 500);
+                    const context = canvas.getContext('2d');
+                    const background = await Canvas.loadImage('./canvasEuro.png');
+                    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+                    context.fillStyle = '#ffffff'
+                    context.font = '35px sans-serif'
+                    context.fillText(textBall, 272, 342)
+                    context.fillText(textStar1, 694, 342)
+                    context.fillText(textStar2, 775, 342)
+
+                    const attachment = new Discord.MessageAttachment(canvas.toBuffer());
+
+                    client.channels.cache.get(channelID).send({ files: [attachment] });
+                }
+            }
         }
-    }
-    
+        else {
+            for (const guildID in data.channelEuro) {
+                const channelID = data.channelEuro[guildID];
+
+                client.channels.cache.get(channelID).send("Pas de nouveau tirage !");
+            }
+        }
+
+    });
 }
 
 client.login(process.env.TOKEN);
